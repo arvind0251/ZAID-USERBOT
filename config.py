3519932650797e1f129fcde482bfa93b54e8cd31
Frozen owner <156554454+prashantsahlot@users.noreply.github.com>
2024-04-25 15:31:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_ID = int(getenv("API_ID", "26495505")) #optional
 API_HASH = getenv("API_HASH", "41ce1aa633c6ae79b928ddd4caefc245") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1668494031").split()))
-OWNER_ID = int(getenv("1668494031"))
+OWNER_ID = int(getenv(""))
 MONGO_URL = getenv("mongodb+srv://BrandedSupportGroup:BRANDED_WORLD@cluster0.v4odcq9.mongodb.net/?retryWrites=true&w=majority")
 BOT_TOKEN = getenv("6265302593:AAGiQvKqmgyunJcicKnfo4tD3CGy09HAlXc", "")
 ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/ee5d441f240fdd88e5a8b.jpg')
